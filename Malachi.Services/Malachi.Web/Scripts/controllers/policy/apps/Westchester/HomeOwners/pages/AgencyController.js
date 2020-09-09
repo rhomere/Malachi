443e1bef4fdf82571a416745c0a75b56eb95aae2
rhomere <rhomere@gmail.com>
2020-09-09 10:07:47 -0400

    remove Malachi.Web project

--- Malachi.Services/Malachi.Web/Scripts/controllers/policy/apps/Westchester/HomeOwners/pages/AgencyController.js
+++
@@ -1,7 +0,0 @@
-﻿'use strict'
-
-/* Setup general page controller */
-MALACHIAPP.controller('test_Homeowners_AgencyController', ['$rootScope', '$scope', '$location', '$stateParams', '$ocLazyLoad', 'notificationsHub', 'settings', 'policyService', function ($rootScope, $scope, $location, $stateParams, $ocLazyLoad, notificationsHub, settings, policyService) {
- 
-}]);
- 
